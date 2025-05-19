221a78c3c160bcffb8731897f5385e980210a758
Radovan Zeman <r.zeman96@gmail.com>
2025-05-19 14:59:12 +0200

    init

---
+++ app/routes/(app)/events/$eventId.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/(app)/events/$eventId")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const { eventId } = Route.useParams();
+
+  return <div>{`Hello from /events/${eventId}`}</div>;
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div>
+      Hello "/"
+      <Button variant="ghost">CLICK ME</Button>
+    </div>
+  );
+}
